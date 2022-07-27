9f6b8a4352587d352dfbaabe7dceb786dc8e91bf
bananasfourscale <lindbergna@gmail.com>
2022-07-27 01:20:16 -0700

    Need to still clean up the way I add the data from summary into the final dict but for now Its at least getting all the data which is dope

--- Parsers/Team_Summary_Parser.py
+++ Parsers/Team_Summary_Parser.py
@@ -20,7 +20,7 @@ team_summary_data = {
     'Florida Panthers' : [],
     'Los Angeles Kings' : [],
     'Minnesota Wild' : [],
-    'Montreal Canadiens' : [],
+    'Montréal Canadiens' : [],
     'Nashville Predators' : [],
     'New Jersey Devils' : [],
     'New York Islanders' : [],
@@ -64,9 +64,11 @@ class summary_indecies(Enum):
     SHA_GP = 65
     FOW_PER = 68
 
+#"C:\\Users\\lindb\\Documents\\chromedriver_win32\\chromedriver.exe"
 
-def scrap_team_summary() -> dict:
-    driver = webdriver.Chrome("P:\\chromedriver.exe")
+
+def scrap_team_summary() -> None:
+    driver = webdriver.Chrome("C:\\Users\\lindb\\Documents\\chromedriver_win32\\chromedriver.exe")
     driver.get("https://www.nhl.com/stats/teams")
     sleep(10)
     pageSource = driver.page_source
@@ -81,14 +83,37 @@ def scrap_team_summary() -> dict:
     source_str_trimmed = source_str_trimmed[0:table_index]
     source_str_trimmed = source_str_trimmed.split('\n')
     index = 0
-    for row in source_str_trimmed:
-        if row.strip() in team_summary_data.keys():
-            print(row)
-    del source_str_trimmed
-    del source_str
-    del soup
-    del pageSource
-    del driver
+    while index < len(source_str_trimmed):
+        if source_str_trimmed[index].strip() in team_summary_data.keys():
+            source_str_trimmed[index].strip()
+            team_summary_data[source_str_trimmed[index].strip()].append([
+                source_str_trimmed[index + summary_indecies.SEASON.value],
+                source_str_trimmed[index + summary_indecies.GP.value],
+                source_str_trimmed[index + summary_indecies.W.value],
+                source_str_trimmed[index + summary_indecies.L.value],
+                source_str_trimmed[index + summary_indecies.SEASON.value],
+                source_str_trimmed[index + summary_indecies.T.value],
+                source_str_trimmed[index + summary_indecies.OT.value],
+                source_str_trimmed[index + summary_indecies.PTS.value],
+                source_str_trimmed[index + summary_indecies.PTS_PER.value],
+                source_str_trimmed[index + summary_indecies.RW.value],
+                source_str_trimmed[index + summary_indecies.ROW.value],
+                source_str_trimmed[index + summary_indecies.SOW.value],
+                source_str_trimmed[index + summary_indecies.GF.value],
+                source_str_trimmed[index + summary_indecies.GA.value],
+                source_str_trimmed[index + summary_indecies.GF_GP.value],
+                source_str_trimmed[index + summary_indecies.GA_GP.value],
+                source_str_trimmed[index + summary_indecies.PP_PER.value],
+                source_str_trimmed[index + summary_indecies.PK_PER.value],
+                source_str_trimmed[index + summary_indecies.NET_PP.value],
+                source_str_trimmed[index + summary_indecies.NET_PK.value],
+                source_str_trimmed[index + summary_indecies.SHF_GP.value],
+                source_str_trimmed[index + summary_indecies.SHA_GP.value],
+                source_str_trimmed[index + summary_indecies.FOW_PER.value]])
+            index += summary_indecies.FOW_PER.value
+        else:
+            index += 1
+
 
 def parse_team_summary(file_name : str = "") -> None:
     header_row = True
@@ -117,3 +142,4 @@ if __name__ == "__main__":
     # parse_team_summary("Input_Files/TeamSummary.csv")
     # print(team_summary_data)
     scrap_team_summary()
+    print(team_summary_data)
